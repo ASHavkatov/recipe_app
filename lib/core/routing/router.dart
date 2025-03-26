fbba08686ec59ad4832509027a88d97e0c9834cb
ASHavkatov <shavkatovasilbek086@gmail.com>
2025-03-27 00:47:40 +0500

    Beckendni uzi tayor

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -38,7 +38,7 @@ import '../../main.dart';
 
 final GoRouter router = GoRouter(
   navigatorKey: navigatorKey,
-  initialLocation: Routes.home,
+  initialLocation: Routes.yourRecipes,
   routes: [
     GoRoute(
         path: Routes.home,
